a2044c9d46fc77dbd640308cc781d89dda607a67
Roman-code-web <selenerg.275@gmail.com>
2023-01-05 01:02:32 -0500

    envio de datos al otro componente c4

--- src/app/componentes/c3/c3.component.ts
+++ src/app/componentes/c3/c3.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-c3',
@@ -6,18 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./c3.component.css']
 })
 export class C3Component {
-  listaTareas:any[]=[]
+  listaTareas:any[]=[];
   mensaje!:string;
   validacion!:boolean;
+  @Output() onEnviarTarea= new EventEmitter();
   guardarTarea(nomTarea:HTMLInputElement,desc:HTMLTextAreaElement){
+    //validacion
     if(nomTarea.value=="" || desc.value==""){
       this.validacion=false;
       this.mensaje="Escribe el nombre y la descripcion";
       return
     }
+    //guardar
     this.listaTareas.push({"nombre": nomTarea.value,"descripcion":desc.value })
+    this.onEnviarTarea.emit(this.listaTareas);
     nomTarea.value="";
     desc.value="";
-    console.log(this.listaTareas);
+
   }
 }
--- src/app/componentes/c4/c4.component.ts
+++ src/app/componentes/c4/c4.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-c4',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./c4.component.css']
 })
 export class C4Component {
-
+  @Input() listaTareaRecibida=[{"nombre":"","descripcion":""}];
+  borrarTarea(i:number){
+    Swal.fire({
+      icon: 'warning',
+      title: '¿Deseas eliminar la tarea?',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, Porfavor!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Eliminado!',
+          'Tu Tarea a sido eliminada',
+          'success'
+        )
+        this.listaTareaRecibida.splice(i,1);
+      }
+      return false;
+    })
+  
+    
+  }
 }
